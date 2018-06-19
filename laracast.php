b986150c86ad34ae38f040df53fff01d2fd78804
Fedor Pestunov <fpestunov@gmail.com>
2018-06-19 16:16:11 +0400

    Add helpers to our App

--- laracast.php
+++ laracast.php
@@ -5,13 +5,16 @@
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 
 require 'vendor/autoload.php';
 
-$app = new Application();
+$app = new Application('Laracast Demo', 'version 1.0');
 
 $app->register('sayHelloTo')
-    ->addArgument('name')
+    ->setDescription('Offer a greeting to the given person.')
+    //->addArgument('name', InputArgument::REQUIRED)
+    ->addArgument('name', InputArgument::OPTIONAL, 'Your name')
     ->setCode(function(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('Hello World');

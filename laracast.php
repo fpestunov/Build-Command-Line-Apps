680862d84472bfb690cfa76e6b6975d44a8bc60b
Fedor Pestunov <fpestunov@gmail.com>
2018-06-21 11:41:41 +0400

    Add Classes

--- laracast.php
+++ laracast.php
@@ -2,23 +2,13 @@
 
 <?php
 
+use Acme\SayHelloCommand;
 use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
 
 require 'vendor/autoload.php';
 
 $app = new Application('Laracast Demo', 'version 1.0');
 
-$app->register('sayHelloTo')
-    ->setDescription('Offer a greeting to the given person.')
-    //->addArgument('name', InputArgument::REQUIRED)
-    ->addArgument('name', InputArgument::OPTIONAL, 'Your name')
-    ->setCode(function(InputInterface $input, OutputInterface $output)
-    {
-        $message = 'Hello, '. $input->getArgument('name') .'!';
-        $output->writeln("<info>{$message}</info>"); // we can use <comment>
-    });
+$app->add(new SayHelloCommand);
 
 $app->run();
